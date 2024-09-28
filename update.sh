cc426e79588a47c71f5589c4dc2489f6c468ac5f
retropikzel <retropikzel@iki.fi>
2024-09-28 13:44:17 +0300

    Added gitginore, modified update script to work as startup script too

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]
 then
     echo 'Behind remote config; updating'
     git pull
-    docker compose up -d --build
 fi
+
+docker-compose up -d --build
 
