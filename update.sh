171899a1dfd5f9de953d35c270baedbcf2d2b86a
retropikzel <retropikzel@iki.fi>
2024-09-29 07:41:30 +0300

    Trying to fix autoupdate

--- update.sh
+++ update.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-git fetch
-if [[ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]]
-then
-    echo 'Behind remote config; updating'
-    git pull
-    docker-compose stop
-fi
-
+git pull
 docker-compose up --build
 
