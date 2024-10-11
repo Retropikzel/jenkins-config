ab8e3aa776e80caf0f2dae85dc0996def511da82
retropikzel <retropikzel@iki.fi>
2024-10-11 11:22:12 +0300

    Update the url in update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git pull
-curl -X POST "https://jenkins.staging.scheme.org/reload-configuration-as-code/?casc-reload-token=${CASC_RELOAD_TOKEN}"
+curl -X POST "https://jenkins.scheme.org/reload-configuration-as-code/?casc-reload-token=${CASC_RELOAD_TOKEN}"
 
